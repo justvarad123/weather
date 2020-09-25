5598ac49ad25faeb734c0d922311e30df7bbe42a
justvarad123 <33244550+justvarad123@users.noreply.github.com>
2020-09-25 16:55:46 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,45 +1,50 @@
 from flask import Flask, render_template, request 
-
-# import json to load JSON data to a python dictionary 
-import json 
-
-# urllib.request to make a request to api 
-import urllib.request 
+# importing requests and json
+import requests, json
+# base URL
 
 
 app = Flask(__name__) 
 
-@app.route('/', methods =['POST', 'GET']) 
-def weather(): 
-	if request.method == 'POST': 
-		city = request.form['city'] 
-	else: 
-		# for default name mathura 
+@app.route('/', methods =['POST', 'GET'])
+def weather():
+	if request.method == 'POST':
+		city = request.form['city']
+	else:
 		city = 'mathura'
-
-	# your API key will come here 
-	api = api_key_here 
-
-	# source contain json data from api 
-	source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q =' + city + '&appid =' + api).read() 
-
-	# converting JSON data to a dictionary 
-	list_of_data = json.loads(source) 
-
-	# data for variable list_of_data 
-	data = { 
-		"country_code": str(list_of_data['sys']['country']), 
-		"coordinate": str(list_of_data['coord']['lon']) + ' '
-					+ str(list_of_data['coord']['lat']), 
-		"temp": str(list_of_data['main']['temp']) + 'k', 
-		"pressure": str(list_of_data['main']['pressure']), 
-		"humidity": str(list_of_data['main']['humidity']), 
-	} 
-	print(data) 
+	BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
+	API_KEY = "c7258453da79906d58e0cf0e26e1ab7a"
+	URL = BASE_URL + "q=" + city + "&appid=" + API_KEY
+	# HTTP request
+	response = requests.get(URL)
+	# checking the status code of the request
+	if response.status_code == 200:
+		# getting data in the json format
+		list_of_data = response.json()
+		report = list_of_data['weather']
+		# data for variable list_of_data
+		temp= list_of_data['main']['temp']- 273.15
+		data = {
+		"tempe": float("{0:.2f}".format(temp)), 
+		"pressure": list_of_data['main']['pressure'], 
+		"humidity": list_of_data['main']['humidity'],
+		"rep": report[0]['description']
+		}
+		print(data)
+	else:
+		print("Error in the HTTP request")
 	return render_template('index.html', data = data) 
-
-
 
 if __name__ == '__main__': 
 	app.run(debug = True) 
+
+		 
+           
+
+
+
+
+
+
+
 
